46e3fbedcf4b33b8d0498aa82b9d77721d9907ee
jacobpelletier <jacobjpelletier@gmail.com>
2018-12-03 21:09:27 -0500

    another attempt at fixing logic of final output

--- PrgHandler.py
+++ PrgHandler.py
@@ -593,8 +593,11 @@ class PrgHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             
         try:   
-            if (finalans.verify_hourstosleep()==True and 
-            isinstance(int(finalans.currentdose()), int)==True):
+            if ((finalans.verify_hourstosleep()) and (finalans.verify_roast())
+                and (finalans.verify_size()) and (finalans.verify_time()) and
+                (finalans.verify_smoker()) and (finalans.verify_age()) and
+                (isinstance(int(finalans.currentdose()), int)==True)):
+                
                 # This input is good.  Remember it.
                 memory["Should you have more coffee?"] = finalans.futuredose()
     
